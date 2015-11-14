c31bf5f976fdd27f35980c2e2456ca043b2c767c
taea <igiari@gmail.com>
2015-11-14 17:10:00 +0900

    Initial commit

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
-source 'https://rubygems.org'
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
 
-ruby '2.1.0'
+gem "middleman", "~>3.2.1"
 
-gem 'middleman', '~> 3.2'
-gem 'puma',      '~> 2.7'
-gem 'rake',      '~> 10.1'
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.1.0"
+
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -1,15 +1,70 @@
-activate :automatic_image_sizes
-activate :directory_indexes
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# activate :livereload
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
 
 set :css_dir, 'stylesheets'
+
 set :js_dir, 'javascripts'
+
 set :images_dir, 'images'
 
+# Build-specific configuration
 configure :build do
-  activate :minify_css
-  activate :minify_javascript
-  activate :asset_hash
-end
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
 
-# silence i18n warning
-::I18n.config.enforce_available_locales = false
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
